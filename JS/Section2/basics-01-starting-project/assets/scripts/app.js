6b9974d32d9b7863d77c1f27be08273eb37987a1
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-08-02 21:43:11 +0200

    add more data types objects and arrays

--- JS/Section2/basics-01-starting-project/assets/scripts/app.js
+++ JS/Section2/basics-01-starting-project/assets/scripts/app.js
@@ -1,14 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets iput from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value)
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
+}
 
 function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '');
-  // alert('The resulst is ' + result);
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries.operation)
+  console.log(logEntries);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function mulitply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
-
-// currentResult = add(1, 2);
-
-// let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', mulitply);
+divideBtn.addEventListener('click', divide);
